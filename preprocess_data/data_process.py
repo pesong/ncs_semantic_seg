f083428d92fe854ef5f063c4b1d0afe9b696a98e
pesong <codeturbo@gmail.com>
2018-08-16 14:57:57 +0800

    add data preprocess

--- preprocess_data/data_process.py
+++ preprocess_data/data_process.py
@@ -1,17 +1,24 @@
 import os
 from PIL import Image
 
-# path_name = '/dl/data/bdd100k/seg/images/test'
-# train_list_file = open('/dl/data/bdd100k/seg/images/test.txt', 'w')
-#
-# for file in os.listdir(path_name):
-#     file_name = str(file).rstrip('.jpg')
-#     train_list_file.write(file_name + '\n')
+path_name = '/media/pesong/e/dl_gaussian/data/cityscapes/cityscapes_ori/leftImg8bit/val'
+target_name = '/media/pesong/e/dl_gaussian/data/cityscapes/cityscapes_ncs/leftImg8bit/val'
+# target_file = '/media/pesong/e/dl_gaussian/data/cityscapes/cityscapes_ncs/val.txt'
+# f = open(target_file, 'w')
 
-path_name = '/media/pesong/e/dl_gaussian/data/bdd100k/seg/images/total_images/'
+for root, dirs, files in os.walk(path_name):
+    for dir in dirs:
 
-for file in os.listdir(path_name):
-    file_path = path_name + str(file)
-    with Image.open(file_path) as img:
-        resized = img.resize((480, 320))
-        resized.save("/media/pesong/e/dl_gaussian/data/bdd100k/seg/images/total_320_480/" + str(file))
+        save_path = target_name + '/' + dir + '/'
+        if not os.path.exists(save_path):
+            os.makedirs(save_path, exist_ok=True)
+
+        dir_path = root + '/' + dir
+        for image_name in os.listdir(dir_path):
+            img_path = dir_path + '/' + str(image_name)
+            # f.write(img_path + '\n')
+            with Image.open(img_path) as img:
+                resized = img.resize((480, 320))
+                resized.save(save_path + str(image_name))
+
+# f.close()
