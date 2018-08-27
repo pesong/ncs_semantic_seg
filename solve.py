55d2fd9a6427e19e3ed7c0865638d16698df8004
pesong <codeturbo@gmail.com>
2018-08-27 20:08:25 +0800

    finetune mobilenetv2 & add pretrained model

--- solve.py
+++ solve.py
@@ -13,9 +13,16 @@ try:
 except:
     pass
 
-weights = 'weight_pretrained/mobilenet_v2_ncs.caffemodel'
-proto = 'weight_pretrained/mobilenet_v2_ncs_deploy.prototxt'
-final_model_name = 'cityscapes_4s_mobilenet_v2'
+# init train use mobilenetv2 ncs v2
+# https://github.com/xufeifeiWHU/Mobilenet-v2-on-Movidius-stick
+# weights = 'weight_pretrained/mobilenet_v2_ncsV2.caffemodel'
+# proto = 'weight_pretrained/mobilenet_v2_ncsV2_deploy.prototxt'
+
+# use out pretrained model
+weights = 'weight_pretrained/mobilenetv2_ncsV2_fcn4s_pretrained.caffemodel'
+proto = 'weight_pretrained/mobilenetv2_ncsV2_fcn4s_pretrained_deploy.prototxt'
+
+final_model_name = 'mobilenetv2_fcn4s_road'
 n_steps = 20000
 
 # init
